42acb7c8395c46aa68dc7d6a7168aa71359fcb6a
AlejandraL205 <62623963+AlejandraL205@users.noreply.github.com>
2024-10-30 20:10:47 -0500

    CAMBIOS

--- Revolt Heroes 2D/Assets/Scripts/jefe.cs
+++ Revolt Heroes 2D/Assets/Scripts/jefe.cs
@@ -2,110 +2,114 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI; // Asegúrate de incluir esta línea para usar UI
 
-public class Boss : MonoBehaviour {
+public class Boss : MonoBehaviour
+{
 
-	public Rigidbody2D bullet;
-	public Transform[] shotSpawners;
-	public float minYForce, maxYForce;
-	public float fireRateMin, fireRateMax;
+    public Rigidbody2D bullet;
+    public Transform[] shotSpawners;
+    public float minYForce, maxYForce;
+    public float fireRateMin, fireRateMax;
 
-	public GameObject enemy;
-	public Transform enemySpawn;
-	public float minEnemyTime, maxEnemyTime;
+    public GameObject enemy;
+    public Transform enemySpawn;
+    public float minEnemyTime, maxEnemyTime;
 
-	public GameObject laser;
-	public Transform laserSpawn;
-	public float minLaserTime, maxLaserTime;
+    public GameObject laser;
+    public Transform laserSpawn;
+    public float minLaserTime, maxLaserTime;
 
-	public int health;
+    public int health;
 
-	private bool isDead = false;
-	private SpriteRenderer sprite;
+    private bool isDead = false;
+    private SpriteRenderer sprite;
 
-	// Use this for initialization
-	void Start () {
+    // Referencia al texto de victoria
+    public GameObject victoryText; // Arrastra aquí el objeto de texto de victoria en el Inspector
 
-		sprite = GetComponent<SpriteRenderer>();
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+        victoryText.SetActive(false); // Asegúrate de que el texto esté desactivado al inicio
+    }
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Update()
+    {
 
-	public void ActivateBoss()
-	{
-		GetComponent<PolygonCollider2D>().enabled = true;
-		Invoke("Fire", Random.Range(fireRateMin, fireRateMax));
-		Invoke("InstantiateEnemies", Random.Range(minEnemyTime, maxEnemyTime));
-		Invoke("FireLaser", Random.Range(minLaserTime, maxLaserTime));
-	}
+    }
 
-	void FireLaser()
-	{
-		if (!isDead)
-		{
-			Instantiate(laser, laserSpawn.position, laserSpawn.rotation);
-			Invoke("FireLaser", Random.Range(minLaserTime, maxLaserTime));
-		}
-	}
+    public void ActivateBoss()
+    {
+        GetComponent<PolygonCollider2D>().enabled = true;
+        Invoke("Fire", Random.Range(fireRateMin, fireRateMax));
+        Invoke("InstantiateEnemies", Random.Range(minEnemyTime, maxEnemyTime));
+        Invoke("FireLaser", Random.Range(minLaserTime, maxLaserTime));
+    }
 
-	void InstantiateEnemies()
-	{
-		if (!isDead)
-		{
-			Instantiate(enemy, enemySpawn.position, enemySpawn.rotation);
-			Invoke("InstantiateEnemies", Random.Range(minEnemyTime, maxEnemyTime));
-		}
-	}
+    void FireLaser()
+    {
+        if (!isDead)
+        {
+            Instantiate(laser, laserSpawn.position, laserSpawn.rotation);
+            Invoke("FireLaser", Random.Range(minLaserTime, maxLaserTime));
+        }
+    }
 
-	void Fire()
-	{
-		if (!isDead)
-		{
-			Rigidbody2D tempBullet = Instantiate(bullet, shotSpawners[Random.Range(0, shotSpawners.Length)].position, Quaternion.identity);
-			tempBullet.AddForce(new Vector2(0, Random.Range(minYForce, maxYForce)), ForceMode2D.Impulse);
-			Invoke("Fire", Random.Range(fireRateMin, fireRateMax));
-		}
-	}
+    void InstantiateEnemies()
+    {
+        if (!isDead)
+        {
+            Instantiate(enemy, enemySpawn.position, enemySpawn.rotation);
+            Invoke("InstantiateEnemies", Random.Range(minEnemyTime, maxEnemyTime));
+        }
+    }
 
-	public void TookDamage(int damage)
-	{
-		health -= damage;
-		if (health <= 0)
-		{
-			isDead = true;
-			Enemy[] enemies = FindObjectsOfType<Enemy>();
-			foreach (Enemy enemy in enemies)
-			{
-				enemy.gameObject.SetActive(false);
-			}
-			Bullet[] bullets = FindObjectsOfType<Bullet>();
-			foreach (Bullet bullet in bullets)
-			{
-				bullet.gameObject.SetActive(false);
-			}
+    void Fire()
+    {
+        if (!isDead)
+        {
+            Rigidbody2D tempBullet = Instantiate(bullet, shotSpawners[Random.Range(0, shotSpawners.Length)].position, Quaternion.identity);
+            tempBullet.AddForce(new Vector2(0, Random.Range(minYForce, maxYForce)), ForceMode2D.Impulse);
+            Invoke("Fire", Random.Range(fireRateMin, fireRateMax));
+        }
+    }
 
-			Invoke("LoadScene", 2f);
-			
-		}
-		else
-		{
-			StartCoroutine(TookDamageCoRoutine());
-		}
-	}
+    public void TookDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            isDead = true;
+            Enemy[] enemies = FindObjectsOfType<Enemy>();
+            foreach (Enemy enemy in enemies)
+            {
+                enemy.gameObject.SetActive(false);
+            }
+            Bullet[] bullets = FindObjectsOfType<Bullet>();
+            foreach (Bullet bullet in bullets)
+            {
+                bullet.gameObject.SetActive(false);
+            }
 
-	IEnumerator TookDamageCoRoutine()
-	{
-		sprite.color = Color.red;
-		yield return new WaitForSeconds(0.1f);
-		sprite.color = Color.white;
-	}
+            victoryText.SetActive(true); // Mostrar el texto de victoria
+            Invoke("LoadScene", 2f);
+        }
+        else
+        {
+            StartCoroutine(TookDamageCoRoutine());
+        }
+    }
 
-	void LoadScene()
-	{
-		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-	}
+    IEnumerator TookDamageCoRoutine()
+    {
+        sprite.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        sprite.color = Color.white;
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 }
